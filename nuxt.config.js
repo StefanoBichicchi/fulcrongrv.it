6b81691580fa56c17d7405a91a037cd77e8e5a1d
StefanoBichicchi <bichicchi.stefano@gmail.com>
2020-10-15 12:10:57 +0200

    add google analytics

--- nuxt.config.js
+++ nuxt.config.js
@@ -46,7 +46,7 @@ export default {
   css: ['~/assets/global.scss'],
 
   // Plugins to run before rendering page (https://go.nuxtjs.dev/config-plugins)
-  plugins: [],
+  plugins: [{ src: '~plugins/ga.js', mode: 'client' }],
 
   // Auto import components (https://go.nuxtjs.dev/config-components)
   components: true,
